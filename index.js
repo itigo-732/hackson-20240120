871a185a9a6525c56d26b26200e904c60b22c60c
aaaa777 <aaaa777@users.noreply.github.com>
2024-01-20 10:45:06 +0900

    シンプルなタブ機能を備えたApp2の作成

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './sample/App2/App2';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
