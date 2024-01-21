27790da65b14600a9b6273bdf6457fca2d123151
Luft256 <luft256@gmail.com>
2024-01-21 13:06:57 +0900

    EditScreen フッター追加 canvas 追加&タッチテスト

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
+//import App from './src/Test/CanvasTest.tsx';
+//import App from './src/Test/CanvasVanillaTest.tsx';
 import App from './App';
 
 AppRegistry.registerComponent(appName, () => App);
